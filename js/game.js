1daeb972f0c02d7bda5d5c82daaf8cb7c8c4b410
unknown <juanpablocapilla@gmail.com>
2021-12-07 18:23:00 -0300

    mas cambios al main

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 //script que crea la pagina de detalle de cada juego
-
+// tiene que estar creado en el panel admin
 let productos = JSON.parse(localStorage.getItem('productos'));
 
 let idProducto = window.location.hash.slice(1);
@@ -8,8 +8,8 @@ console.log(prod);
 
 let paginaDetalle = document.createElement('div');
 paginaDetalle.innerHTML = `
-<h1>${prod.nombre}</h1>
-<p>${prod.nombre}</p>
+<h3>${prod.nombre}</h3>
+<p>${prod.description}</p>
 <p>${prod.precio}</p>
 `;
 let padre = document.getElementById('padre');
