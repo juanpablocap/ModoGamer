f3740ff1c17406028ea841dd82544e8a302923a2
Carlos Mocsary <carlos.mocsary@gmail.com>
2021-12-15 10:38:28 -0300

    refactorizando

--- JS/admin.js
+++ JS/admin.js
@@ -1,11 +1,12 @@
 class Game {
-    constructor(id, name, category, description, published, image) {
+    constructor(id, name, category, description, published, image, outstanding) {
         this.id = id;
         this.name = name;
         this.category = category;
         this.description = description;
         this.published = published;
         this.image = image;
+        this.outstanding = outstanding
     }
 }
 
@@ -16,7 +17,8 @@ let games = [
         "Estrategia",
         "Uno de los juegos de estrategia en tiempo real más queridos vuelve a demostrar todo su esplendor con Age of Empires IV, donde serás la estrella de las épicas batallas históricas que moldearon el mundo que hoy conocemos.",
         false,
-        "age_of_empire_IV.png"
+        "age_of_empire_IV.png", "age_of_empire_IV.png",
+        false
     ),
     new Game(
         2,
@@ -24,7 +26,8 @@ let games = [
         "Mundo abierto",
         "Con más de 175 premios al Juego del año y más de 250 valoraciones perfectas, Red Dead Redemption 2 es una historia épica sobre el honor y la lealtad en el umbral de una nueva era. Incluye Red Dead Redemption 2: Modo Historia y Red Dead Online.",
         true,
-        "red_dead_redemption_II.jpg"
+        "red_dead_redemption_II.jpg",
+        false
     ),
     new Game(
         3,
@@ -32,7 +35,8 @@ let games = [
         "Simulacion",
         "Ponte en la piel de un granjero de hoy en día y construye tu granja en tres entornos americanos y europeos. Farming Simulator 22 ofrece una gran variedad de elementos y se centra en la agricultura, la cría de animales y la silvicultura. Ahora, además, ¡se han añadido los ciclos por estaciones!",
         true,
-        "farming_simulator_22.jfif"
+        "farming_simulator_22.jfif",
+        false
     ),
     new Game(
         4,
@@ -40,7 +44,8 @@ let games = [
         "Mundo abierto",
         "Te damos la bienvenida a Yara, un paraíso tropical que se ha detenido en el tiempo. El dictador de Yara, Antón Castillo, está decidido a devolver a su nación la antigua gloria perdida por cualquier medio, y su hijo Diego sigue sus sangrientos pasos. La opresión a la que someten a su pueblo ha dado pie a una revolución.",
         true,
-        "far_cry_6.jpg"
+        "far_cry_6.jpg",
+        false
     ),
     new Game(
         5,
@@ -48,7 +53,8 @@ let games = [
         "Acción",
         "Kratos y Atreus se embarcan en una mítica aventura en busca de respuestas y aliados antes de la llegada del Ragnarök.",
         false,
-        "god_of_war_ragnarok.jpg"
+        "god_of_war_ragnarok.jpg",
+        false
     ),
     
 ];
@@ -61,12 +67,11 @@ if (!localStorage.getItem("games")) {
 mostrarJuegosTabla();
 
 function mostrarJuegosTabla() {
-    let data = localStorage.getItem("games");
-    let gamesLS = JSON.parse(data);
     
+    let gamesLS = getLocalS();
     gamesLS.forEach((game) => {
-       
         let data = document.createElement("tr");
+        
         data.innerHTML = `
         <th class="col-sm-2 col-md-2 col-lg-1">Codigo</th>
         <td class="col-sm-2 col-md-2 col-lg-1">Nombre</td>
@@ -80,24 +85,24 @@ function mostrarJuegosTabla() {
         <td class="col-sm-3 col-md-3 col-lg-1">${game.category}</td>
         <td class="col-sm-5 col-md-5 col-lg-3">${(game.description.length>120) ? game.description.substr(0, 120)+"..." : game.description}</td>
         <td class="col-sm-2 col-md-2 col-lg-1"><div>
-        <button onclick="publicado(${game.id})" id="btnPublicado${game.id}"></button>
+        <button onclick="publicado(${game.id})" id="publishedBtn${game.id}"></button>
         </div></td>
         <td class="col-sm-2 col-md-2 col-lg-1">
         <section class="d-flex flex-column justify-content-center align-items-center btnOpciones">
         <div class="opacityIMG_Button imgHover">
-        <button onclick="eliminarJuego(${game.id})" class="btn btn-danger m-1"><i class="fas fa-trash-alt"></i></button>
+        <button onclick="eliminarJuego(${game.id})" class="btn btn-danger m-1" title="Eliminar juego"><i class="fas fa-trash-alt"></i></button>
         </div>
         <div class="opacityIMG_Button imgHover">
-        <button onclick="datosJuegoMod(${game.id})" class="btn btn-primary m-1" data-bs-toggle="modal" data-bs-target="#adminModal"><i class="fas fa-edit"></i></button>
+        <button onclick="datosJuegoMod(${game.id})" class="btn btn-primary m-1" data-bs-toggle="modal" data-bs-target="#adminModal" title="Modificar juego"><i class="fas fa-list-alt"></i></button>
         </div>
         <div class="opacityIMG_Button imgHover">
-        <button onclick="destacarJuego(${game.id})" class="btn btn-warning m-1"><i class="fas fa-medal"></i></button>
+        <button onclick="destacado(${game.id})" class="btn btn-secondary m-1" id="outstandingBtn${game.id}" title="Destacar juego"><i class="fas fa-medal"></i></button>
         </div>
         </section>
         </td>
         <td class="col-sm-8 col-md-8 col-lg-4">
-        <div class="opacityIMG_Button">
-        <img src="../IMAGENES/imgGames/${game.image}" class="img-fluid imgJuego">
+        <div class="opacityIMG_Button" >
+        <img src="../IMAGENES/imgGames/${game.image}" class="img-fluid imgJuego" alt="${game.image}">
         </div>
         </td>
         `
@@ -105,54 +110,17 @@ function mostrarJuegosTabla() {
         //data.style="color: color :hover blueviolet";
         let table = document.querySelector("#cuerpoTabla");
         table.appendChild(data);
-        colorPublicado(game.published, game.id);
+        colorButton(game.published, game.id, "published");
+        colorButton(game.outstanding, game.id, "outstanding");
+        
         
     });
 }
 
-function crearModalMod(id) {
-
-    let modalModificar = document.createElement("div");
-    modalModificar.innerHTML = `
-    <div class="modal" id="modalModificar" tabindex="-1">   
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                <h5 class="modal-title">Modificar juego</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                <form class="d-flex flex-column" action="" method="post">
-                                    <label class="text-center">ID - ${id}</label>
-                                    <label for="gameNameMod">Nombre</label>
-                                    <input type="text" name="gameNameMod" id="gameNameMod">
-                                    <label for="gameCategoryMod">Categoria</label>
-                                    <input type="text" name="gameCategoryMod" id="gameCategoryMod">
-                                    <label for="gameDescriptionMod">Descripción</label>
-                                    <input type="text" name="gameDescriptionMod" id="gameDescriptionMod">
-                                    <label for="gamePublishedMod">Publicado</label>
-                                    <input type="checkbox" name="gamePublishedMod" id="gamePublishedMod">
-                                    <label for="gameImageURLMod">URL Imagen</label>
-                                    <input type="url" name="gameImageURLMod" id="gameImageURLMod">
-                                </form>
-                                </div>
-                                <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                <button type="button" onclick="modificarJuego(${id})" class="btn btn-primary">Guardar cambios</button>
-                                </div>
-                            </div>    
-                        </div>
-                    </div>
-    `
-    let modal = document.querySelector("body");
-    modal.appendChild(modalModificar);
-
-}
-
 function agregarJuego() {
 
-    let data = localStorage.getItem("games");
-    let gamesLS = JSON.parse(data);
+    let gamesLS = getLocalS();
+
     console.log(document.getElementById("gameID").value);
     gamesLS.forEach(game => {
         if(game.id == parseInt(document.getElementById("gameID").value)){
@@ -161,9 +129,9 @@ function agregarJuego() {
             let category = document.getElementById("gameCategory").value;
             let description = document.getElementById("gameDescription").value;
             let published = document.getElementById("gamePublished").checked ? true : false;
+            let outstanding = document.getElementById("gameOutstanding").checked ? true : false;
             let image = document.getElementById("gameImageURL").value;
 
-            //console.log(id);
             console.log(game.id);
 
             game.name = name;
@@ -171,6 +139,7 @@ function agregarJuego() {
             game.description = description;
             game.published = published;
             game.image = image;
+            game.outstanding = outstanding;
 
             data = JSON.stringify(gamesLS);
             localStorage.setItem("games", data);
@@ -186,9 +155,10 @@ function agregarJuego() {
         let category = document.getElementById("gameCategory").value;
         let description = document.getElementById("gameDescription").value;
         let published = document.getElementById("gamePublished").checked ? true : false;
+        let outstanding = document.getElementById("gameOutstanding").checked ? true : false;
         let image = document.getElementById("gameImageURL").value;
 
-        let newGame = new Game(id, name, category, description, published, image);
+        let newGame = new Game(id, name, category, description, published, image, outstanding);
 
         gamesLS.push(newGame);
 
@@ -200,8 +170,8 @@ function agregarJuego() {
 }
 
 function eliminarJuego(id) {
-    let games = localStorage.getItem("games");
-    let gamesLS = JSON.parse(games);
+    
+    let gamesLS = getLocalS();
 
     Swal.fire({
         title: "¿Eliminar juego?",
@@ -221,7 +191,7 @@ function eliminarJuego(id) {
                         icon: 'success',
                         title: 'Juego eliminado',
                         showConfirmButton: false,
-                        timer: 1150
+                        timer: 1100
                     });    
                 }
                 //window.location.assign(window.location.origin + "/admin.html");
@@ -233,8 +203,7 @@ function eliminarJuego(id) {
 
 function datosJuegoMod(id) { 
 
-    let data = localStorage.getItem("games");
-    let gamesLS = JSON.parse(data);
+    let gamesLS = getLocalS();
 
     gamesLS.forEach(game => {
         if(game.id == id){
@@ -243,19 +212,20 @@ function datosJuegoMod(id) {
             document.getElementById('gameCategory').value = game.category;
             document.getElementById('gameDescription').value = game.description;
             document.getElementById('gamePublished').checked = game.published;
+            document.getElementById('gameOutstanding').checked = game.outstanding;
             document.getElementById('gameImageURL').value = game.image;
         }
     });
 }
 
-function destacarJuego(id) { }
-
 function publicado(id) {
-    let games = localStorage.getItem("games");
-    let gamesLS = JSON.parse(games);
+    
+    let gamesLS = getLocalS();
     
     gamesLS.forEach((game) => {
-        if (game.id == id) {
+        
+        if (game.id === id) {
+            
             if (game.published == false) {
                 Swal.fire({
                     title: "¿Publicar?",
@@ -268,15 +238,15 @@ function publicado(id) {
                         game.published = true;
                         data = JSON.stringify(gamesLS);
                         localStorage.setItem("games", data);
-                        let btnP = document.getElementById("btnPublicado"+id);
-                        btnP.className = "btn btn-primary";
+                        let btnP = document.getElementById("publishedBtn"+id);
+                        btnP.className = "btn btn-success";
 
                         Swal.fire({
                             position: 'center',
                             icon: 'success',
                             title: 'Juego publicado',
                             showConfirmButton: false,
-                            timer: 1150
+                            timer: 1100
                         });         
                     }
                 });
@@ -292,7 +262,7 @@ function publicado(id) {
                         game.published = false;
                         data = JSON.stringify(gamesLS);
                         localStorage.setItem("games", data);
-                        let btnP = document.getElementById("btnPublicado"+id);
+                        let btnP = document.getElementById("publishedBtn"+id);
                         btnP.className = "btn btn-danger";
 
                         Swal.fire({
@@ -300,7 +270,7 @@ function publicado(id) {
                             icon: 'warning',
                             title: 'Juego sin publicar',
                             showConfirmButton: false,
-                            timer: 1150
+                            timer: 1100
                         });                      
                     }
                 });
@@ -309,14 +279,75 @@ function publicado(id) {
     });   
 }
 
-function colorPublicado(colorPublished, id) {
+function destacado(id) {
     
-    if (colorPublished) {   
-        let btnP = document.getElementById("btnPublicado"+id);
-        btnP.className = "btn btn-primary";
+    let gamesLS = getLocalS();
+    
+    gamesLS.forEach((game) => {
+        
+        if (game.id === id) {
+            
+            if (game.outstanding == false) {
+                Swal.fire({
+                    title: "¿Destacar juego?",
+                    showDenyButton: false,
+                    showCancelButton: true,
+                    confirmButtonText: "Guardar",
+                }).then((result) => {
+                    if (result.isConfirmed) {
+
+                        game.outstanding = true;
+                        data = JSON.stringify(gamesLS);
+                        localStorage.setItem("games", data);
+                        let btnD = document.getElementById("outstandingBtn"+id);
+                        btnD.className = "btn btn-warning";
+
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'success',
+                            title: 'Juego destacado',
+                            showConfirmButton: false,
+                            timer: 1100
+                        });         
+                    }
+                });
+            } else {
+                Swal.fire({
+                    title: "¿No destacar?",
+                    showDenyButton: false,
+                    showCancelButton: true,
+                    confirmButtonText: "Guardar",
+                }).then((result) => {
+                    if (result.isConfirmed) {
+
+                        game.outstanding = false;
+                        data = JSON.stringify(gamesLS);
+                        localStorage.setItem("games", data);
+                        let btnD = document.getElementById("outstandingBtn"+id);
+                        btnD.className = "btn btn-secondary";
+
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'warning',
+                            title: 'Juego sin destacar',
+                            showConfirmButton: false,
+                            timer: 1100
+                        });                      
+                    }
+                });
+            }
+        }
+    });   
+}
+
+function colorButton(color, id, tipoBoton) {
+    
+    if (color) {
+        let btnC = document.getElementById(tipoBoton+"Btn"+id);
+        (tipoBoton == "published")? btnC.className = "btn btn-success" : btnC.className = "btn btn-warning";
     } else {
-        let btnP = document.getElementById("btnPublicado"+id);
-        btnP.className = "btn btn-danger";
+        let btnC = document.getElementById(tipoBoton+"Btn"+id);
+        (tipoBoton == "published")? btnC.className = "btn btn-danger" : btnC.className = "btn btn-secondary";
     } 
 }
 
@@ -326,11 +357,12 @@ function limpiarCamposModal (){
     document.getElementById("gameCategory").value = "";
     document.getElementById("gameDescription").value = "";
     document.getElementById("gamePublished").checked = false;
+    document.getElementById("gameOutstanding").checked = false;
     document.getElementById("gameImageURL").value = "";
 }
 
-function limitarCaracteres(){
-    if(game.description.length > 120){
-        limite = game.description.substr(0, 120)+"...";
-     }
+function getLocalS(){
+    let games = localStorage.getItem("games");
+    let gamesLS = JSON.parse(games);
+    return gamesLS;
 }
